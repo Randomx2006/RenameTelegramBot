48d848faa3f18b14cb25296950eecb77afe239d4
Randomx2006 <ransome458@gmail.com>
2025-05-04 20:56:01 +0530

    Update bot_users.py

--- RenameBot/bot_users.py
+++ RenameBot/bot_users.py
@@ -1,21 +1,20 @@
-from RenameBot.database.users_sql import Users, num_users
-from RenameBot.database import SESSION
+from pymongo import MongoClient
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from Config import MONGO_DB_URI
 
+client = MongoClient(MONGO_DB_URI)
+db = client['RenameBotDB']
+users_col = db['users']
 
 @Client.on_message(~filters.edited & ~filters.service, group=1)
-async def users_sql(_, msg: Message):
+async def add_user_to_db(_, msg: Message):
     if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
-            SESSION.add(Users(msg.from_user.id))
-            SESSION.commit()
-        else:
-            SESSION.close()
-
+        user_id = msg.from_user.id
+        if not users_col.find_one({"_id": user_id}):
+            users_col.insert_one({"_id": user_id})
 
 @Client.on_message(filters.user(1946995626) & ~filters.edited & filters.command("stats"))
-async def _stats(_, msg: Message):
-    users = num_users()
-    await msg.reply(f"Total Users : {users}", quote=True)
+async def show_stats(_, msg: Message):
+    count = users_col.count_documents({})
+    await msg.reply(f"Total Users : {count}", quote=True)
