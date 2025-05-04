456b86841f04e17e52ce7983730b2c38a844cb92
Randomx2006 <ransome458@gmail.com>
2025-05-04 21:50:08 +0530

    Update bot_users.py

--- RenameBot/bot_users.py
+++ RenameBot/bot_users.py
@@ -8,13 +8,16 @@ client = MongoClient(MONGO_DB_URI)
 db = client['RenameBotDB']
 users_col = db['users']
 
-@Client.on_message(~filters.edited & ~filters.service, group=1)
+@Client.on_message(~filters.service, group=1)
 async def add_user_to_db(_, msg: Message):
+    # Skip if edited
+    if msg.edit_date:
+        return
+
     if msg.from_user:
         user_id = msg.from_user.id
         if not users_col.find_one({"_id": user_id}):
             users_col.insert_one({"_id": user_id})
-
 @Client.on_message(filters.user(1946995626) & ~filters.edited & filters.command("stats"))
 async def show_stats(_, msg: Message):
     count = users_col.count_documents({})
