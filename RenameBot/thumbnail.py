4f688c09350f3615a43bb67b8ffd1b30299d1a56
Randomx2006 <ransome458@gmail.com>
2025-05-04 23:07:22 +0530

    Update thumbnail.py

--- RenameBot/thumbnail.py
+++ RenameBot/thumbnail.py
@@ -1,28 +1,42 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
-from RenameBot.database import SESSION
-from RenameBot.database.users_sql import Users
+from motor.motor_asyncio import AsyncIOMotorClient
 from io import BytesIO
+import os
 
+# MongoDB config (make sure to set MONGO_URI in your config)
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["rename_bot"]
+users_collection = db["users"]
 
 @Client.on_message(filters.private & filters.command(["thumbnail", "tn"]) & ~filters.edited)
 async def _thumbnail(_, msg: Message):
-    q = SESSION.query(Users).get(msg.from_user.id)
-    if q.thumbnail_status:
-        switch = "On"
-    else:
+    user_id = msg.from_user.id
+    user = await users_collection.find_one({"_id": user_id})
+
+    if not user:
+        await users_collection.insert_one({
+            "_id": user_id,
+            "thumbnail": None,
+            "thumbnail_status": False
+        })
         switch = "Off"
-    if q.thumbnail:
-        thumbnail = q.thumbnail
+        thumbnail = None
+    else:
+        switch = "On" if user.get("thumbnail_status", False) else "Off"
+        thumbnail = user.get("thumbnail")
+
+    if thumbnail:
         thumbnail = BytesIO(thumbnail)
         thumbnail.name = "image.jpg"
         await msg.reply_photo(
             thumbnail,
             caption="This is the current thumbnail",
             reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("✨ Change Thumbnail ✨", callback_data="change_tn")],
-                    [InlineKeyboardButton("✨ Remove Thumbnail ✨", callback_data="remove_tn")],
-                    [InlineKeyboardButton(f"Thumbnail Status : {switch}", callback_data="tn_status_change")]
+                [InlineKeyboardButton("✨ Change Thumbnail ✨", callback_data="change_tn")],
+                [InlineKeyboardButton("✨ Remove Thumbnail ✨", callback_data="remove_tn")],
+                [InlineKeyboardButton(f"Thumbnail Status : {switch}", callback_data="tn_status_change")]
             ])
         )
     else:
@@ -32,4 +46,3 @@ async def _thumbnail(_, msg: Message):
                 [InlineKeyboardButton("✨ Add Thumbnail ✨", callback_data="add_tn")]
             ])
         )
-    SESSION.close()
