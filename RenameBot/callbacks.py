868bc0c332e1c09ac39d5e9ecba426f2e2c1b01f
Randomx2006 <ransome458@gmail.com>
2025-05-04 22:52:30 +0530

    Update callbacks.py

--- RenameBot/callbacks.py
+++ RenameBot/callbacks.py
@@ -2,8 +2,7 @@ import shutil
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from Data import Data
-from RenameBot.database import users_col
-from RenameBot.database.users_sql import Users
+from RenameBot.database import users_col  # Ensure users_col is a pymongo collection
 
 
 async def image_to_binary(image):
@@ -12,70 +11,70 @@ async def image_to_binary(image):
     return binary
 
 
-# Callbacks
 @Client.on_callback_query()
 async def _callbacks(bot: Client, callback_query: CallbackQuery):
     user = await bot.get_me()
-    mention = user["mention"]
+    mention = user.mention
     query = callback_query.data.lower()
+    user_id = callback_query.from_user.id
+
     if query == "home":
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.message_id
         await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
+            chat_id=user_id,
+            message_id=callback_query.message.message_id,
             text=Data.START.format(callback_query.from_user.mention, mention),
             reply_markup=InlineKeyboardMarkup(Data.buttons),
         )
+
     elif query == "about":
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.message_id
         await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
+            chat_id=user_id,
+            message_id=callback_query.message.message_id,
             text=Data.ABOUT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
+
     elif query == "help":
-        chat_id = callback_query.from_user.id
-        message_id = callback_query.message.message_id
         await bot.edit_message_text(
-            chat_id=chat_id,
-            message_id=message_id,
+            chat_id=user_id,
+            message_id=callback_query.message.message_id,
             text="**Here's How to use me**\n" + Data.HELP,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
+
     elif query in ["add_tn", "change_tn"]:
         await callback_query.message.delete()
-        image_message = await bot.ask(callback_query.from_user.id, "Send a image to set as thumbnail",
-                                      filters=filters.photo)
-        await bot.send_message(callback_query.from_user.id, "Wait...Downloading and Saving...")
+        image_message = await bot.ask(user_id, "Send an image to set as thumbnail", filters=filters.photo)
+        await bot.send_message(user_id, "Wait...Downloading and Saving...")
         image = await image_message.download()
         binary = await image_to_binary(image)
-        q = SESSION.query(Users).get(callback_query.from_user.id)
-        q.thumbnail = binary
-        q.thumbnail_status = True
-        SESSION.commit()
-        await image_message.reply("Thumbnail Set !", quote=True)
+        users_col.update_one(
+            {"_id": user_id},
+            {"$set": {"thumbnail": binary, "thumbnail_status": True}},
+            upsert=True
+        )
+        await image_message.reply("Thumbnail Set!", quote=True)
         shutil.rmtree("downloads")
+
     elif query == "remove_tn":
-        q = SESSION.query(Users).get(callback_query.from_user.id)
-        q.thumbnail = ""
-        q.thumbnail_status = False
-        SESSION.commit()
+        users_col.update_one(
+            {"_id": user_id},
+            {"$set": {"thumbnail": None, "thumbnail_status": False}}
+        )
         await callback_query.message.delete()
-        await bot.send_message(callback_query.from_user.id, "Thumbnail Removed !")
+        await bot.send_message(user_id, "Thumbnail Removed!")
+
     elif query == "tn_status_change":
-        q = SESSION.query(Users).get(callback_query.from_user.id)
-        if q.thumbnail_status:
-            q.thumbnail_status = False
-            switch = "Off"
-        else:
-            q.thumbnail_status = True
-            switch = "On"
-        SESSION.commit()
+        user_data = users_col.find_one({"_id": user_id}) or {}
+        status = not user_data.get("thumbnail_status", False)
+        users_col.update_one(
+            {"_id": user_id},
+            {"$set": {"thumbnail_status": status}},
+            upsert=True
+        )
+        switch = "On" if status else "Off"
         await callback_query.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton("Change Thumbnail", callback_data="change_tn")],
@@ -83,17 +82,17 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
                 [InlineKeyboardButton(f"Thumbnail Status : {switch}", callback_data="tn_status_change")]
             ])
         )
+
     elif query == "video_to_setting":
-        q = SESSION.query(Users).get(callback_query.from_user.id)
-        if q.video_to.lower() == "video":
-            q.video_to = "document"
-            now = "Document"
-        else:
-            q.video_to = "video"
-            now = "Video"
+        user_data = users_col.find_one({"_id": user_id}) or {}
+        new_format = "document" if user_data.get("video_to", "video") == "video" else "video"
+        users_col.update_one(
+            {"_id": user_id},
+            {"$set": {"video_to": new_format}},
+            upsert=True
+        )
         await callback_query.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(f"Video to : {now}", callback_data="video_to_setting")]
+                [InlineKeyboardButton(f"Video to : {new_format.capitalize()}", callback_data="video_to_setting")]
             ])
         )
-        SESSION.commit()
