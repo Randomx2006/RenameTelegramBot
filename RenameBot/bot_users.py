1071dfe8f0b8e12f007ac42cfb03b71200d6b1c0
Randomx2006 <ransome458@gmail.com>
2025-05-04 22:02:27 +0530

    Update bot_users.py

--- RenameBot/bot_users.py
+++ RenameBot/bot_users.py
@@ -2,11 +2,13 @@ from pymongo import MongoClient
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import Config
+
 MONGO_DB_URI = Config.DATABASE_URL
 
 client = MongoClient(MONGO_DB_URI)
 db = client['RenameBotDB']
 users_col = db['users']
+
 
 @Client.on_message(~filters.service, group=1)
 async def add_user_to_db(_, msg: Message):
@@ -18,7 +20,13 @@ async def add_user_to_db(_, msg: Message):
         user_id = msg.from_user.id
         if not users_col.find_one({"_id": user_id}):
             users_col.insert_one({"_id": user_id})
-@Client.on_message(filters.user(1946995626) & ~filters.edited & filters.command("stats"))
+
+
+@Client.on_message(filters.user(1946995626) & filters.command("stats"))
 async def show_stats(_, msg: Message):
+    # Skip if edited
+    if msg.edit_date:
+        return
+
     count = users_col.count_documents({})
     await msg.reply(f"Total Users : {count}", quote=True)
