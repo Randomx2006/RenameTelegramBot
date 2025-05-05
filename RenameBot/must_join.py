2057a7b322048167453b9eb73afb98536ed5a32c
Randomx2006 <ransome458@gmail.com>
2025-05-05 14:43:45 +0530

    Update must_join.py

--- RenameBot/must_join.py
+++ RenameBot/must_join.py
@@ -6,7 +6,7 @@ from config import Config
 MUST_JOIN = Config.MUST_JOIN
 
 
-@Client.on_message(filters=(~filters.edited & ~filters.service & filters.user & filters.incoming), group=-1)
+@Client.on_message(filters=(~filters.edited_messages & ~filters.service & filters.user & filters.incoming), group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     try:
         try:
