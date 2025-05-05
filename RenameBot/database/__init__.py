6c183caebe536241e2133104ef03aea411601874
Randomx2006 <ransome458@gmail.com>
2025-05-05 15:11:21 +0530

    Update __init__.py

--- RenameBot/database/__init__.py
+++ RenameBot/database/__init__.py
@@ -1,8 +1,16 @@
 from pymongo import MongoClient
 from config import Config
 
+# MongoDB connection URI from config
 MONGO_DB_URI = Config.DATABASE_URL
 
-client = MongoClient(MONGO_DB_URI)
-db = client['RenameBotDB']  # You can change this to any DB name you want
+# Create and expose a global MongoDB client session
+SESSION = MongoClient(MONGO_DB_URI)
+
+# Select your database
+db = SESSION['RenameBotDB']
+
+# Collections you plan to use
 users_col = db['users']
+files_col = db['files']  # if storing file metadata
+settings_col = db['settings']  # optional for bot settings
